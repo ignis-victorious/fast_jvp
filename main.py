6dda823a4c0ad616a7abfdc05e95306523218188
Emmanuele Levi <ignisvictorious@gmail.com>
2025-04-23 16:29:22 +0100

    add path parameters

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #  ___________________
 #  Import LIBRARIES
 from fastapi import FastAPI
+
 #  Import FILES
+from models import FoodEnum
 #  ___________________
 
 
@@ -21,6 +23,34 @@ async def post() -> dict[str, str]:
 @app.put("/")
 async def put() -> dict[str, str]:
     return {"message": "hello from the put route"}
+
+
+@app.get("/users")
+async def list_users() -> dict[str, str]:
+    return {"message": "list users route"}
+
+
+@app.get("/users/me")
+async def get_current_user() -> dict[str, str]:
+    return {"Message": "this is the current user"}
+
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: str) -> dict[str, str]:
+    return {"user_id": user_id}
+
+
+@app.get("/foods/{food_name}")
+async def get_food(food_name: FoodEnum) -> dict[str, str]:
+    if food_name == FoodEnum.vegetables:
+        return {"food_name": food_name, "message": "you are healthy"}
+
+    if food_name.value == "fruits":
+        return {
+            "food_name": food_name,
+            "message": "you are still healthy, but like sweet things",
+        }
+    return {"food_name": food_name, "message": "i like chocolate milk"}
 
 
 # This is not needed at the moment
---
+++ models.py
@@ -0,0 +1,11 @@
+#  ___________________
+#  Import LIBRARIESI
+from enum import Enum
+#  Import FILES
+#  ___________________
+
+
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
